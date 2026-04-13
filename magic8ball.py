6799c338f2eab412c01baf024d87fd929dab3ad1
farooghnaush <farooghnaushahi@gmail.com>
2026-04-13 21:12:10 +0200

    Magic 8 ball program

---
+++ magic8ball.py
@@ -0,0 +1,14 @@
+import random
+
+class magic8ball:
+    def throwdice(self, n):
+        text=["It is certain","It is decidedly so","Without a doubt","Yes definitely","You may rely on it","As I see it, yes","Most likely","Outlook good","Yes","Signs point to yes","Reply hazy, try again","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again","Don't count on it","My reply is no","My sources say no","Outlook not so good","Very doubtful"]
+        self.n = random.choice(text)
+        return self.n
+
+print("Enter a question")
+c = input()
+
+obj = magic8ball()
+d = obj.throwdice(c)
+print(d)
